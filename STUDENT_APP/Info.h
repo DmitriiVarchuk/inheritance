d6fe3fc128c6c17a4fafac487b8db031c1537849
Dmytro Varchuk <loltop792@gmail.com>
2025-02-11 13:35:24 +0200

    Add files via upload

---
+++ STUDENT_APP/Info.h
@@ -0,0 +1,17 @@
+#ifndef INFO_H
+#define INFO_H
+
+#include <string>
+
+class Info {
+public:
+    std::string name;
+    std::string surname;
+    std::string email;
+    std::string phone;
+    std::string address;
+    std::string city;
+    std::string country;
+};
+
+#endif // INFO_H
